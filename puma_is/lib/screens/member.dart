3ec84c9361a7d4b24299a9e56aa05d9739457f89
yasuarin <dadiaputu@gmail.com>
2024-12-17 15:51:32 +0700

    new member

--- puma_is/lib/screens/member.dart
+++ puma_is/lib/screens/member.dart
@@ -14,7 +14,7 @@ class _MemberPageState extends State<MemberPage> {
   String _currentFilter = 'All';
   ScrollController _scrollController = ScrollController();
   bool _isLoadingMore = false;
-  int _itemsLimit = 13; 
+  int _itemsLimit = 13;
 
   final Map<String, String> memberImages = {
     'John Doe': 'assets/images/johndoe.jpg',
@@ -32,22 +32,34 @@ class _MemberPageState extends State<MemberPage> {
     'Jacky Witeen': 'assets/images/jackywiteen.jpeg',
   };
 
+  @override
+  void initState() {
+    super.initState();
+    _fetchMembersByDivision(_currentFilter);
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   void _fetchMembersByDivision(String division) async {
     try {
       var memberQuery = FirebaseFirestore.instance.collection('Members');
       var memberSnapshot = await memberQuery.get();
 
       List<DocumentSnapshot> allMembers = memberSnapshot.docs;
-      List<DocumentSnapshot> filteredMembers = allMembers
-          .where((doc) {
-            var memberData = doc.data() as Map<String, dynamic>;
-            return division == 'All' || memberData['division'] == division;
-          })
-          .toList();
+      List<DocumentSnapshot> filteredMembers = allMembers.where((doc) {
+        var memberData = doc.data() as Map<String, dynamic>;
+        return division == 'All' || memberData['division'] == division;
+      }).toList();
 
       setState(() {
         _allMembers = allMembers;
-        _filteredMembers = filteredMembers; 
+        _filteredMembers = filteredMembers.take(_itemsLimit).toList();
       });
     } catch (e) {
       print('Error fetching members: $e');
@@ -67,20 +79,6 @@ class _MemberPageState extends State<MemberPage> {
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _fetchMembersByDivision(_currentFilter);
-    _scrollController.addListener(_scrollListener);
-  }
-
-  @override
-  void dispose() {
-    _scrollController.removeListener(_scrollListener);
-    _scrollController.dispose();
-    super.dispose();
-  }
-
   void _scrollListener() {
     if (_scrollController.position.pixels ==
             _scrollController.position.maxScrollExtent &&
@@ -96,120 +94,10 @@ class _MemberPageState extends State<MemberPage> {
     Future.delayed(const Duration(seconds: 2), () {
       setState(() {
         _isLoadingMore = false;
+        _itemsLimit += 10;
         _filteredMembers = _allMembers.take(_itemsLimit).toList();
       });
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        elevation: 0, 
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: _updateMemberFilter,
-            itemBuilder: (BuildContext context) {
-              return {
-                'All',
-                'BoD',
-                'Public Relation',
-                'Art and Sport',
-                'Student Development'
-              }.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(
-                    choice,
-                    style: TextStyle(
-                      color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.white 
-                          : Colors.black,
-                    ),
-                  ),
-                );
-              }).toList();
-            },
-            icon: Icon(
-              Icons.filter_list,
-              color: Theme.of(context).brightness == Brightness.dark
-                  ? Colors.white 
-                  : Colors.black, 
-            ),
-          ),
-        ],
-      ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          int crossAxisCount = 2; 
-          double childAspectRatio = 1.0; 
-
-          return SingleChildScrollView( 
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Center(
-                    child: Text(
-                      _currentFilter == 'All'
-                          ? 'All Members'
-                          : 'Members in $_currentFilter Division',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white 
-                            : Colors.black, 
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                _filteredMembers.isEmpty
-                    ? const Center(
-                        child: Text(
-                          'No Members Found',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      )
-                    : GridView.builder(
-                        controller: _scrollController,
-                        physics: NeverScrollableScrollPhysics(), 
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: crossAxisCount, 
-                          crossAxisSpacing: 10,
-                          mainAxisSpacing: 10,
-                          childAspectRatio: childAspectRatio,
-                        ),
-                        itemCount: _filteredMembers.length,
-                        itemBuilder: (context, index) {
-                          var memberDoc = _filteredMembers[index];
-                          var member = memberDoc.data() as Map<String, dynamic>;
-                          String fullName = member['fullName'] ?? 'No Name';
-                          String batch = _convertToString(member['batch']);
-                          String position = _convertToString(member['position']);
-                          String division = member['division'] ?? 'No Division';
-                          String imagePath = memberImages[fullName] ?? 
-                              'assets/images/default.jpg';
-
-                          return _buildMemberCard(
-                              fullName, batch, position, division, imagePath);
-                        },
-                      ),
-                if (_isLoadingMore)
-                  const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
   }
 
   String _convertToString(dynamic value) {
@@ -241,7 +129,7 @@ class _MemberPageState extends State<MemberPage> {
                 style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: isDarkMode ? Colors.white : Colors.black, 
+                  color: isDarkMode ? Colors.white : Colors.black,
                 ),
                 textAlign: TextAlign.center,
                 maxLines: 2,
@@ -262,14 +150,125 @@ class _MemberPageState extends State<MemberPage> {
                 'Batch: $batch\nPosition: $position\nDivision: $division',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 14, 
+                  fontSize: 14,
                   color: isDarkMode ? Colors.white70 : Colors.black54,
                 ),
               ),
-              const SizedBox(height: 36), 
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        elevation: 0,
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: _updateMemberFilter,
+            itemBuilder: (BuildContext context) {
+              return {
+                'All',
+                'BoD',
+                'Public Relation',
+                'Art and Sport',
+                'Student Development'
+              }.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(
+                    choice,
+                    style: TextStyle(
+                      color: Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                  ),
+                );
+              }).toList();
+            },
+            icon: Icon(
+              Icons.filter_list,
+              color: Theme.of(context).brightness == Brightness.dark
+                  ? Colors.white
+                  : Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          int crossAxisCount = 2;
+          double childAspectRatio = 1.0;
+
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Text(
+                      _currentFilter == 'All'
+                          ? 'All Members'
+                          : 'Members in $_currentFilter Division',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                _filteredMembers.isEmpty
+                    ? const Center(
+                        child: Text(
+                          'No Members Found',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      )
+                    : GridView.builder(
+                        controller: _scrollController,
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: crossAxisCount,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          childAspectRatio: childAspectRatio,
+                        ),
+                        itemCount: _filteredMembers.length,
+                        itemBuilder: (context, index) {
+                          var memberDoc = _filteredMembers[index];
+                          var member = memberDoc.data() as Map<String, dynamic>;
+                          String fullName = member['fullName'] ?? 'No Name';
+                          String batch = _convertToString(member['batch']);
+                          String position = _convertToString(member['position']);
+                          String division = member['division'] ?? 'No Division';
+                          String imagePath = memberImages[fullName] ??
+                              'assets/images/default.jpg';
+
+                          return _buildMemberCard(
+                              fullName, batch, position, division, imagePath);
+                        },
+                      ),
+                if (_isLoadingMore)
+                  const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
